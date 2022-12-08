675b0994174039531ceee3bb84957a225d644fa6
Mahmoud elali <Mahmoudelali@github.com>
2022-12-08 12:05:27 +0200

    JS basics 2 Step_3

--- javascript-basics-02/step 3/step_3.js
+++ javascript-basics-02/step 3/step_3.js
@@ -1,10 +1,18 @@
 let userName = document.getElementById("name");
 let target = document.getElementById("target");
 
+
+let targetDiv = document.createElement('div')
+targetDiv.id = "target"
+document.forms[0].appendChild(targetDiv)
+
+
+
+
 document.forms[0].onsubmit = (e) => {
     e.preventDefault()
 }
 userName.addEventListener("change", (e) => {
     console.log('fires')
-    target.innerHTML = userName.value;
+    targetDiv.innerHTML = userName.value;
 });
